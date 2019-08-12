68edcf323562390cf93dec65a9376484a86e2fd9
YashwanthYarala <yarala.yashwanthreddy1905@gmail.com>
2019-08-12 23:35:53 +0530

    firebase connection added

--- login.js
+++ login.js
@@ -1,10 +1,29 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCE9dkPSWV1NL9xnJiDsbneg7t6mnHui1o",
+    authDomain: "loginpage-fe9ca.firebaseapp.com",
+    databaseURL: "https://loginpage-fe9ca.firebaseio.com",
+    projectId: "loginpage-fe9ca",
+    storageBucket: "loginpage-fe9ca.appspot.com",
+    messagingSenderId: "475414581393",
+    appId: "1:475414581393:web:94103714f19984bf"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  var data = firebase.database();
 $(document).ready(function(){
     var username ;
     var password;
     $('#btn1').click(function(){
-        var username = document.getElementById("username").value;
-        var password = document.getElementById("password").value;
-       
+         username = document.getElementById("username").value;
+         password = document.getElementById("password").value;
+
+         data.ref().on('value',function(snap){
+             var uname = snap.val().username;
+             alert(uname);
+             data.ref().set({username:2});
+         });
+
+         
        
     });
 })
