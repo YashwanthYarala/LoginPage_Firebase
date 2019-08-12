79055b5546b313c5ef1d00b95d9e6c6cbd1f077d
YashwanthYarala <yarala.yashwanthreddy1905@gmail.com>
2019-08-12 23:11:54 +0530

    js file

--- login.js
+++ login.js
@@ -1,13 +1,10 @@
-
-
-
 $(document).ready(function(){
     var username ;
     var password;
     $('#btn1').click(function(){
         var username = document.getElementById("username").value;
         var password = document.getElementById("password").value;
-       alert(username);
+       
        
     });
 })
